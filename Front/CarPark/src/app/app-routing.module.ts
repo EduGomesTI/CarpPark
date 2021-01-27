05624b8c2101729f8fb013f237af1fcd439d2180
EduGomeTI <edugomesti@outlook.com>
2021-01-27 11:06:48 -0300

    Refatorado cadastrar e pesquisar. Rotas

--- Front/CarPark/src/app/app-routing.module.ts
+++ Front/CarPark/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PesquisarComponent } from './pages/pesquisar/pesquisar.component';
+import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
+import { MeuPerfilComponent } from './pages/meu-perfil/meu-perfil.component'
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'pesquisar', component: PesquisarComponent },
+  { path: 'cadastrar', component: CadastrarComponent },
+  { path: 'perfil', component: MeuPerfilComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
